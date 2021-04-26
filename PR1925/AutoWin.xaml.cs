2dcca0fda2017c443d2ae1789cdc8387f5c77ed0
Катя Сиротина <sirotinak0@gmail.com>
2021-04-27 01:59:49 +0700

    fix

---
+++ PR1925/AutoWin.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PR1925
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UsersEntities usersEntities = new UsersEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void signUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckData();
+        }
+
+        private void regButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegWindow regWindow = new RegWindow();
+            regWindow.ShowDialog();
+        }
+
+        public void CheckData()
+        {
+
+            var check= usersEntities.users.FirstOrDefault(x => x.login == loginTextBox.Text && x.password == passwordTextBox.Password);
+            if (check == null)
+            {
+                MessageBox.Show("Неверно введен логин или пароль");
+            }
+            else
+            {
+                UsersWindow usersWindow = new UsersWindow(check);
+                usersWindow.Show();
+               this.Close();
+            }
+        }
+    }
+}
---
+++ PR1925/ChangeWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR1925
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeWindow.xaml
+    /// </summary>
+    public partial class ChangeWindow : Window
+    {
+        UsersEntities context = new UsersEntities();
+        public ChangeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(CheckData());
+            //if
+            var profile = new users { login = loginTextBox.Text, password = passwordTextBox.Password, email = emailTextBox.Text, full_name = fullNameTextBox.Text};
+            context.users.Add(profile);
+            context.SaveChanges();
+            UsersWindow usersWin = new UsersWindow(profile);
+            usersWin.Show();
+            this.Close();
+        }
+        public string CheckData()
+        {
+            char[] signs = new char[6] { '!', '@', '#', '$', '%', '^' };
+            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+            string password = passwordTextBox.Password;
+            bool capital = password.Any(Char.IsUpper);
+            bool c = password.Any(Char.IsDigit);
+            string result = "";
+            for (int i = 0; i < signs.Length; i++)
+            {
+                if (!password.Contains(signs[i]))
+                {
+                    result += "Отсутствует хотя бы 1 специальный символ\n";
+                    break;
+                }
+            }
+            if (password.Length < 6)
+            {
+                result += "Пароль меньше 6 символов\n";
+            }
+            if (capital == false)
+            {
+                result += "Отсутствует хотя бы 1 прописная буква\n";
+            }
+            if (c == false)
+            {
+                result += "Отсутствует хотя бы 1 цифра\n";
+            }
+            if (loginTextBox.Text != "")
+            {
+                result += "Отстутствует логин\n";
+            }
+            string email = emailTextBox.Text;
+            if (!Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase))
+            {
+                result += "Email не подтвержден\n";
+            }
+            return result;
+        }
+    }
+}
